03bf87e3c29315d819473af2fc0e1305a2de33fc
SEONGYEON KIM <83098550+seongyeon1@users.noreply.github.com>
2024-09-06 11:40:08 +0900

    Time: 2171 ms (25.41%), Space: 12.6 MB (17.25%) - LeetHub

--- seongyeon/NeetCode/0001-two-sum/0001-two-sum.py
+++ seongyeon/NeetCode/0001-two-sum/0001-two-sum.py
@@ -1,5 +1,10 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 if nums[i]+nums[j] == target:
