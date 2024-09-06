96b71c0500cc087221a47f8b7da83736763ac877
Junghoon Lee <169136233+coronarita1991@users.noreply.github.com>
2024-09-06 18:50:41 +0900

    Time: 820 ms (6.2%), Space: 27.3 MB (91.44%) - LeetHub

--- gurri/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ gurri/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
@@ -9,43 +8,3 @@ class Solution:
             min_price = min(price, min_price)
             ans = max(ans, price - min_price) 
         return ans 
-=======
-import heapq
-
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        
-        '''
-        Prices = [7, 1, 5, 3, 6, 4]
-        Output: 5
-        
-        given an array "prices" - prices[i] given stock on the ith day.
-        choosing a different day in the future to sell that stock.
-
-        Maximum profit / not achieve - 0
-
-        Two pointer ? 
-        - must buy before you sell
-
-        '''
-        ans = 0 # maximum profit
-        buy = 0
-        # sol 1 - O(n2) - Timeout 
-        # for i, buy in enumerate(prices):
-        #     for j in range(i+1, len(prices)):
-        #         # Can sell ? 
-        #         if prices[j] > buy : 
-        #             gap = prices[j] - buy
-        #             # print(gap)
-        #             ans = max(gap, ans)
-
-        # Sol2 : 역순으로 구하기 
-        ans = 0
-        max_price = 0
-
-        for price in reversed(prices):
-            max_price = max(price, max_price)
-            ans = max(ans, max_price - price)
-
-        return ans
->>>>>>> upstream/main
