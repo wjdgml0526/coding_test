3c9e8ffe99b8972f0af343362876506fb48c9c66
Junghoon Lee <169136233+coronarita1991@users.noreply.github.com>
2024-09-06 09:06:05 +0900

    Time: 36 ms (93.25%), Space: 17.2 MB (40.69%) - LeetHub

--- gurri/0125-valid-palindrome/0125-valid-palindrome.py
+++ gurri/0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,12 +2,7 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
 
         res = "".join([
-<<<<<<< HEAD
             c for c in s if c.isalnum()
         ]).lower()
-=======
-            c.lower() for c in s if c.isalnum()
-        ])
->>>>>>> upstream/main
 
         return True if res == res[::-1] else False
