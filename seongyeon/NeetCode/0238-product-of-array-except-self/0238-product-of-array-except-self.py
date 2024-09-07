9563891b8835d116b604570eab5bf096c8ca493d
SEONGYEON KIM <83098550+seongyeon1@users.noreply.github.com>
2024-09-07 16:27:01 +0900

    Time: 287 ms (51.35%), Space: 19.1 MB (61.97%) - LeetHub

--- seongyeon/NeetCode/0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ seongyeon/NeetCode/0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,30 +1,24 @@
 from functools import reduce
 
 class Solution(object):
+    def multiply(self, arr):
+        return reduce(lambda x,y : x*y, arr)
 
     def productExceptSelf(self, nums):
         """
         :type nums: List[int]
         :rtype: List[int]
         """
-        def multiply(arr):
-            return reduce(lambda x,y : x*y, arr)
-            
-        result = []
-        all_ = multiply(nums)
-        if nums.count(0) >= 1:
-            ans = [0 for _ in range(len(nums))]
-            if nums.count(0) == 1:
-                i = nums.index(0)
-                ans[i] = multiply(nums[:i] + nums[i+1 :])
-            return ans
-        else:
-            return [all_ //n for n in nums]
-            
+        n = len(nums)
+        ans = []
+        result = 1
+        for i in range(n):
+            ans.append(result)
+            result *= nums[i]
+        
+        result = 1
+        for i in reversed(range(n)):
+            ans[i] *= result
+            result *= nums[i]
 
-
-
-            tmp = nums[:i] + nums[i+1 :]
-            result.append(multiply(tmp))
-        return result
-            
+        return ans 
