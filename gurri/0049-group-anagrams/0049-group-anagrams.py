7bd29c95d769eec29b5552ae0fa6649de7c4622c
Junghoon Lee <169136233+coronarita1991@users.noreply.github.com>
2024-09-08 19:19:18 +0900

    Time: 81 ms (90.41%), Space: 20.3 MB (46.78%) - LeetHub

--- gurri/0049-group-anagrams/0049-group-anagrams.py
+++ gurri/0049-group-anagrams/0049-group-anagrams.py
@@ -5,7 +5,8 @@ class Solution:
         for s in strs :
             # key 검사
             k = ''.join(sorted(s))
-            if k not in anag.keys() :
+            if k not in anag : 
+            # if k not in anag.keys() : # 78ms
                 anag[k] = [s]
             else :
                 anag[k].append(s)
