523fc1e6c6ed82852733dccce08dd571789712bd
Junghoon Lee <169136233+coronarita1991@users.noreply.github.com>
2024-09-12 22:17:50 +0900

    Time: 50 ms (5.57%), Space: 16.7 MB (68.98%) - LeetHub

--- gurri/0078-subsets/0078-subsets.py
+++ gurri/0078-subsets/0078-subsets.py
@@ -1,18 +1,21 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        
-        outputs = [[]] # 공집합
+        ans = []
+        selected = []
 
-        for num in nums : 
-            newSubsets = []
-            # outputs를 순회하면서 num을 삽입
-            for currSubset in outputs : # [] 
-                tmp = currSubset.copy() # 복사
-                tmp.append(num)          # [num]
-                newSubsets.append(tmp)   # [[num]]
-            # 결과물 더해주기
-            for newSubset in newSubsets:
-                outputs.append(newSubset)
-        
-        return outputs
+        def backtracking(curr_idx):
+            if curr_idx >= len(nums):
+                print(selected)
+                ans.append(selected.copy())    
+                return
 
+            for i in range(curr_idx, len(nums)):
+                if selected not in ans : 
+                    selected.append(nums[i])
+                    backtracking(i+1)
+                    selected.pop()
+                    backtracking(i+1)
+
+        backtracking(0)
+
+        return ans
