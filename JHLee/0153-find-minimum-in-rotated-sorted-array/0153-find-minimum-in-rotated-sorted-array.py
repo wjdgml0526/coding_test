42db915f5db7ef70f9d9ac20d00b93879b883dba
wjdgml0526 <82493052+wjdgml0526@users.noreply.github.com>
2024-09-13 21:13:13 +0900

    Time: 40 ms (5.02%), Space: 11.8 MB (87.12%) - LeetHub

--- JHLee/0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ JHLee/0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -4,12 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        lp, rp = 0, len(nums) - 1
-        while lp < rp:
-            mid = lp + ((rp - lp) // 2)
-
-            if nums[mid] > nums[rp]:
-                lp = mid + 1
+        start , end = 0, len(nums) - 1 
+        curr_min = float("inf")
+        
+        while start  <  end :
+            mid = start + (end - start ) // 2
+            curr_min = min(curr_min,nums[mid])
+            
+            # right has the min 
+            if nums[mid] > nums[end]:
+                start = mid + 1
+                
+            # left has the  min 
             else:
-                rp = mid
-        return nums[lp]
+                end = mid - 1 
+                
+        return min(curr_min,nums[start])
