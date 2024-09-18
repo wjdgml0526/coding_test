a9dd76f411726cc70e42defa15df50d3d5026ee9
Junghoon Lee <169136233+coronarita1991@users.noreply.github.com>
2024-09-18 20:10:39 +0900

    Time: 58 ms (13.8%), Space: 16.4 MB (87.02%) - LeetHub

--- gurri/0994-rotting-oranges/0994-rotting-oranges.py
+++ gurri/0994-rotting-oranges/0994-rotting-oranges.py
@@ -7,31 +7,27 @@ class Solution:
         
         m, n = len(grid), len(grid[0])
         queue = deque()
-        fresh_oranges = 0
-        
-        # Initialize the queue with all initially rotten oranges
+        fresh = 0
+
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 2:
-                    queue.append((i, j, 0))  # (row, col, time)
+                    queue.append((i,j,0))
                 elif grid[i][j] == 1:
-                    fresh_oranges += 1
-        
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+                    fresh += 1
+
+        dxs, dys = [0,1,0,-1],[1,0,-1,0]
         max_time = 0
-        
+
         while queue:
             x, y, time = queue.popleft()
-            for dx, dy in directions:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == 1:
+            for dx, dy in zip(dxs,dys):
+                nx, ny = x+dx, y+dy
+                if 0<=nx<m and 0<=ny<n and grid[nx][ny] == 1:
                     grid[nx][ny] = 2
-                    fresh_oranges -= 1
-                    queue.append((nx, ny, time + 1))
-                    max_time = max(max_time, time + 1)
-        
-        # If there are still fresh oranges left, return -1
-        if fresh_oranges > 0:
+                    fresh -= 1
+                    queue.append((nx, ny, time+1))
+                    max_time = max(max_time, time+1)
+        if fresh >0:
             return -1
-        
         return max_time
