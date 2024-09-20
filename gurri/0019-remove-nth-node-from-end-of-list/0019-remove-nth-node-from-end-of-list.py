4d41bf200c76c02d72837bb274de1f6b4dd29bf5
Junghoon Lee <169136233+coronarita1991@users.noreply.github.com>
2024-09-20 22:58:25 +0900

    Time: 27 ms (97.14%), Space: 16.6 MB (21.85%) - LeetHub

--- gurri/0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ gurri/0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,23 +5,19 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        
+
         dummy = ListNode()
-        # dummy - head 제거를 용이하게 하려고
         dummy.next = head
-        # n번째를 제거 -> n-1, n+1을 이어주는 아이디어
-        # 투포인터를 활용
         first = dummy
         second = dummy
 
-        for i in range(n+1):
+        for _ in range(n+1):
             second = second.next
-
+        
         while second is not None:
             first = first.next
             second = second.next
-        
-        first.next = first.next.next
 
+        first.next = first.next.next
+        
         return dummy.next
-        # return head #로 하면 안된다. head제거가 안된다.
