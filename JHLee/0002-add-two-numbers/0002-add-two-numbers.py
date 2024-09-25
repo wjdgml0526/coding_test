6720dc08b7d2dacd11e5da7624d100f8a24d9688
wjdgml0526 <82493052+wjdgml0526@users.noreply.github.com>
2024-09-25 16:36:25 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- JHLee/0002-add-two-numbers/0002-add-two-numbers.py
+++ JHLee/0002-add-two-numbers/0002-add-two-numbers.py
@@ -10,23 +10,14 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        dummy = ListNode()
-        cur = dummy
+        cur = dummy = ListNode()
 
         carry = 0
         while l1 or l2 or carry:
-            v1 = l1.val if l1 else 0
-            v2 = l2.val if l2 else 0
+            v1 = l1.val if l1
+            v2 = l2.val if l2
 
-            # new digit
             val = v1 + v2 + carry
             carry = val // 10
             val = val % 10
             cur.next = ListNode(val)
-
-            # update ptrs
-            cur = cur.next
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-
-        return dummy.next
