c9c071d370df5e97de590c4266380a471165df65
wjdgml0526 <82493052+wjdgml0526@users.noreply.github.com>
2024-09-27 23:10:30 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ JHLee/0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        res = 0
+
+        def dfs(root):
+            global res
+
+            if not root:
+                return 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+            res = max(res, left + right)
+
+            return 1 + max(left, right)
+
+        dfs(root)
+        return res
