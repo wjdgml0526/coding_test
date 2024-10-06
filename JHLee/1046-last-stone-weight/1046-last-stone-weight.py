4cff19667d47c3b9f375d77247ed72af67f50f91
wjdgml0526 <82493052+wjdgml0526@users.noreply.github.com>
2024-10-06 19:51:04 +0900

    Time: 18 ms (40.02%), Space: 11.7 MB (53.38%) - LeetHub

---
+++ JHLee/1046-last-stone-weight/1046-last-stone-weight.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: int
+        """
+        stones = [-s for s in stones]
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+            first = heapq.heappop(stones)
+            second = heapq.heappop(stones)
+            if second > first:
+                heapq.heappush(stones, first - second)
+
+        stones.append(0)
+        return abs(stones[0])
