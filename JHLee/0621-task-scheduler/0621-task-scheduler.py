a70ad5302f48fe74d7215ad7f1bb72e8110cf396
wjdgml0526 <82493052+wjdgml0526@users.noreply.github.com>
2024-10-07 21:26:00 +0900

    Time: 419 ms (69.75%), Space: 13.7 MB (59.3%) - LeetHub

---
+++ JHLee/0621-task-scheduler/0621-task-scheduler.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def leastInterval(self, tasks, n):
+        """
+        :type tasks: List[str]
+        :type n: int
+        :rtype: int
+        """
+        count = Counter(tasks)
+        maxHeap = [-cnt for cnt in count.values()]
+        heapq.heapify(maxHeap)
+
+        time = 0
+        q = deque()  # pairs of [-cnt, idleTime]
+        while maxHeap or q:
+            time += 1
+
+            if not maxHeap:
+                time = q[0][1]
+            else:
+                cnt = 1 + heapq.heappop(maxHeap)
+                if cnt:
+                    q.append([cnt, time + n])
+            if q and q[0][1] == time:
+                heapq.heappush(maxHeap, q.popleft()[0])
+        return time
