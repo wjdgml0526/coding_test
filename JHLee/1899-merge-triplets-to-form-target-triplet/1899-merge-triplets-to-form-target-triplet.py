4c2ccb009332cf599088cff26aab7a2c89ff384c
wjdgml0526 <82493052+wjdgml0526@users.noreply.github.com>
2024-10-15 14:30:37 +0900

    Time: 1563 ms (57.98%), Space: 59.1 MB (90.43%) - LeetHub

---
+++ JHLee/1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def mergeTriplets(self, triplets, target):
+        """
+        :type triplets: List[List[int]]
+        :type target: List[int]
+        :rtype: bool
+        """
+        good = set()
+
+        for t in triplets:
+            if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]:
+                continue
+            for i, v in enumerate(t):
+                if v == target[i]:
+                    good.add(i)
+        return len(good) == 3
