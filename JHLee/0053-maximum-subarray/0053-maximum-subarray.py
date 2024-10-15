ecaf83c25ff2cef14f9db9b12ddd893b5f412f69
wjdgml0526 <82493052+wjdgml0526@users.noreply.github.com>
2024-10-15 14:07:47 +0900

    Time: 519 ms (83.62%), Space: 24.6 MB (38.61%) - LeetHub

---
+++ JHLee/0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = nums[0]
+
+        total = 0
+        for n in nums:
+            total += n
+            res = max(res, total)
+            if total < 0:
+                total = 0
+        return res
